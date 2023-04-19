80629d488ebdb1eea00039f9a3c1995eb5ce9b56
Giao Ngo <giaongo.97@gmail.com>
2023-04-19 22:52:37 +0300

    Add system println for case condition

--- sketch_spaceshipInterface/sketch_spaceshipInterface.ino
+++ sketch_spaceshipInterface/sketch_spaceshipInterface.ino
@@ -6,17 +6,20 @@ void setup() {
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(2, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   switchState = digitalRead(2);
   if (switchState == LOW) {
-    // The button is not pressed 
+
+    Serial.println("Button is not pressed");
     digitalWrite(3, HIGH); //green led
     digitalWrite(4, LOW); // red led
     digitalWrite(5, LOW); // red led
   } else {
+    Serial.println("Button is pressed");
     digitalWrite(3,LOW);
     digitalWrite(4,LOW);
     digitalWrite(5,HIGH);
